ac8d7e3b76f899317a4fc7bb6c22ec41c4b06c2f
Jorge Botarro <chikinrasshu@icloud.com>
2024-05-25 18:23:28 -0400

    Memory sizes macros

--- include/chk/core/mem.h
+++ include/chk/core/mem.h
@@ -4,4 +4,9 @@
 
 void* chk_allocate(Size amount);
 void* chk_reallocate(void* ptr, Size new_size);
-void chk_free(void* ptr);
+void  chk_free(void* ptr);
+
+#define chk_kilobytes(N) (1024ULL * (N))
+#define chk_megabytes(N) (1024ULL * chk_kilobytes(N))
+#define chk_gigabytes(N) (1024ULL * chk_megabytes(N))
+#define chk_terabytes(N) (1024ULL * chk_gigabytes(N))
