7f7f445b5914f1f313041641931dfc739c499e9a
Jorge Botarro <chikinrasshu@icloud.com>
2024-05-25 17:25:49 -0400

    Fix arena's lack of out variable

--- include/chk/core/arena.h
+++ include/chk/core/arena.h
@@ -13,6 +13,7 @@ bool chk_arena_destroy(Arena* a);
 bool chk_arena_reset(Arena* a);
 bool chk_arena_push(Arena* a, Size size, void** out);
 
-#define chk_arena_push_struct(arena, Type) chk_arena_push((arena), sizeof(Type))
-#define chk_arena_push_array(arena, Type, count)                               \
-    chk_arena_push((arena), sizeof(Type) * (count))
+#define chk_arena_push_struct(arena, Type, out)                                \
+    chk_arena_push((arena), sizeof(Type), (out))
+#define chk_arena_push_array(arena, Type, count, out)                          \
+    chk_arena_push((arena), sizeof(Type) * (count), (out))
